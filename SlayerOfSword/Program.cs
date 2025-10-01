24d1c821641d2684175c94a6bb58b137c8cdca1e
heegwon <gnrms0507@gmail.com>
2025-10-01 09:27:44 +0900

    MakeMainPage
    메인페이지 제작

---
+++ SlayerOfSword/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Media;
+using System.Threading;
+
+namespace SlayerOfSword
+{
+
+    internal class Program
+    {
+        /*
+           * 검의용사 키우기 (턴제 RPG) 제목 Slayer of sword
+           */
+        static void Main(string[] args)
+        {
+            GameManger gm = new GameManger();   //GM생성
+
+
+
+
+
+
+
+
+        }
+    }
+}
